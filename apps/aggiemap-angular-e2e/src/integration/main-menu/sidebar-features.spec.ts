2242f604181d2dbcf646a83aac9e8041a1ebe432
minhb121 <minh.a.bui97@gmail.com>
2021-10-26 18:50:15 -0500

    removed the 's' from 'https' to run aggiemap locally

--- apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
@@ -5,7 +5,7 @@ desktopSizes.forEach((size) => {
   describe(`Sidebar Features on ${size} Resolution`, () => {
     beforeEach(() => {
       cy.viewport(size[0], size[1]);
-      cy.visit('https://localhost:4200'); // setup to run aggiemap locally
+      cy.visit('http://localhost:4200'); // setup to run aggiemap locally
       cy.get('canvas').should('be.visible');
     });
     // Tests begin here
