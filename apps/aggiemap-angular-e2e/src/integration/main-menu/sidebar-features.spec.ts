7b635dc6dc36ccd7cf1cf1f07b517915c0f8e87d
minhb121 <minh.a.bui97@gmail.com>
2021-10-26 20:50:56 -0500

    added test that should close side bar after double clicking directions and bus route tab

--- apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
@@ -7,12 +7,24 @@ desktopSizes.forEach((size) => {
       cy.viewport(size[0], size[1]);
       cy.visit('http://localhost:4200'); // setup to run aggiemap locally
       cy.get('canvas').should('be.visible');
+      cy.get('.tabs-content-container').should('be.visible');
     });
-    // Tests begin here
-    it(`Should Close Sidebar`, () => {
+    // Tests begin here *delete when submitting pull request*
+    it(`Should Close Sidebar on Features Tab Click`, () => {
       cy.get('[ng-reflect-title="Features"] > .esri-component > div > .material-icons').click();
       cy.get('.tabs-content-container').should('not.be.visible');
     });
-    // Tests end here
+
+    it(`Should Close Sidebar on Directions Tab Double Click`, () => {
+      cy.get('[ng-reflect-title="Directions"] > .esri-component > div > .material-icons').dblclick();
+      cy.get('.tabs-content-container').should('not.be.visible');
+    });
+
+    it(`Should Close Sidebar on Bus Routes Tab Double Click`, () => {
+      cy.get('.ng-star-inserted > .esri-component > div > .material-icons').dblclick();
+      cy.get('.tabs-content-container').should('not.be.visible');
+    });
+
+    // Tests end here *delete when submitting pull request*
   });
 });
