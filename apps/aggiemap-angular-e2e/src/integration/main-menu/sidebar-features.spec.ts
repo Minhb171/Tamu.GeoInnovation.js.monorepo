03f8c4f7d2d3ebb0da3cb2ae18146ae3baa3c20c
minhb121 <minh.a.bui97@gmail.com>
2021-11-03 17:57:16 -0500

    altered selectors to current tests to prevent flaky tests

--- apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/sidebar-features.spec.ts
@@ -7,30 +7,33 @@ desktopSizes.forEach((size) => {
       cy.viewport(size[0], size[1]);
       cy.visit('http://localhost:4200'); // setup to run aggiemap locally
       cy.get('canvas').should('be.visible');
-      cy.get('.tabs-content-container').should('be.visible');
+      cy.get('[data-cy=sidebar-content-container]').should('be.visible');
+      cy.get('[data-cy=tabs]').should('be.visible');
     });
+
     // Tests begin here *delete when submitting pull request*
     it(`Should Close Sidebar on Features Tab Click`, () => {
-      cy.get('[ng-reflect-title="Features"] > .esri-component > div > .material-icons').click();
-      cy.get('.tabs-content-container').should('not.be.visible');
+      cy.get('[ng-reflect-title="Features"]').click();
+      cy.get('[data-cy=sidebar-content-container]').should('not.be.visible');
     });
 
     it(`Should Close Sidebar on Directions Tab Double Click`, () => {
-      cy.get('[ng-reflect-title="Directions"] > .esri-component > div > .material-icons').dblclick();
-      cy.get('.tabs-content-container').should('not.be.visible');
+      cy.get('[ng-reflect-title="Directions"]').dblclick();
+      cy.get('[data-cy=sidebar-content-container]').should('not.be.visible');
     });
 
     it(`Should Close Sidebar on Bus Routes Tab Double Click`, () => {
-      cy.get('.ng-star-inserted > .esri-component > div > .material-icons').dblclick();
-      cy.get('.tabs-content-container').should('not.be.visible');
+      cy.get('[ng-reflect-title="Bus Routes"]').dblclick();
+      cy.get('[data-cy=sidebar-content-container]').should('not.be.visible');
     });
+
     // test will pass once features tab click function is resolved (I'm assuming)
     it(`Should Open Sidebar on Features Tab Double Click`, () => {
-      cy.get('[ng-reflect-title="Features"] > .esri-component > div > .material-icons').dblclick();
-      cy.get('tamu-gisc-sidebar.ng-tns-c76-0.ng-trigger-slide.right')
-        .should('have.attr', 'style')
-        .and('contain', 'translateX(0px)');
+      cy.get('[ng-reflect-title="Features"]').dblclick();
+      cy.get('tamu-gisc-sidebar').should('have.attr', 'style').and('contain', 'translateX(0px)');
     });
+
+    it('Should Switch Between Sidebar Menus', () => {});
     // Tests end here *delete when submitting pull request*
   });
 });
