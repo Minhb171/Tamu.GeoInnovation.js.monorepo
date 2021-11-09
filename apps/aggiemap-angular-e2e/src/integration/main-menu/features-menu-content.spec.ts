ad63e8b7ff138a9971555887d4eec823126ca7a7
minhb121 <minh.a.bui97@gmail.com>
2021-11-09 13:37:21 -0600

    added additional assertions for visibility of features menu items

--- apps/aggiemap-angular-e2e/src/integration/main-menu/features-menu-content.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/features-menu-content.spec.ts
@@ -1,20 +1,27 @@
 import { desktopSizes } from '../../support/resolutions';
 
 desktopSizes.forEach((size) => {
-  describe(`Features Menu Content on ${size} Resolution`, () => {
+  describe(`Display Features Menu Content on ${size} Resolution`, () => {
     beforeEach(() => {
       cy.viewport(size[0], size[1]);
       cy.visit('http://localhost:4200'); // setup to run aggiemap locally
       cy.get('canvas').should('be.visible');
     });
 
-    it('Should Display Content for Features Menu', () => {
+    it('Should Display Search Bar', () => {
       cy.get('[data-cy=building-search-bar]').should('be.visible');
+    });
+
+    it('Should Display Layer list', () => {
       cy.contains('Layers').should('be.visible');
-      cy.contains('Legend').should('be.visible');
-      // change selectors from here down
       cy.get('tamu-gisc-layer-list').should('be.visible');
+      cy.get('tamu-gisc-layer-list-item').should('be.visible');
+    });
+
+    it.only('Should Display Legend List', () => {
+      cy.contains('Legend').should('be.visible');
       cy.get('tamu-gisc-legend').should('be.visible');
+      cy.get('.legend-info').should('be.visible');
     });
   });
 });
