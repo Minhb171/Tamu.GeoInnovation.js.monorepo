6b2f681a10d4b76e13d8274859781faa45f4b06b
minhb121 <minh.a.bui97@gmail.com>
2021-11-16 11:07:31 -0600

    Added additional assertions to ensure elements are not empty

--- apps/aggiemap-angular-e2e/src/integration/main-menu/features-menu-content.spec.ts
+++ apps/aggiemap-angular-e2e/src/integration/main-menu/features-menu-content.spec.ts
@@ -9,19 +9,22 @@ desktopSizes.forEach((size) => {
     });
 
     it('Should Display Search Bar', () => {
-      cy.get('[data-cy=building-search-bar]').should('be.visible');
+      cy.get('[data-cy=building-search-bar]')
+        .should('be.visible')
+        .and('to.be.empty')
+        .and('contain.attr', 'Placeholder', 'Find Building or Parking');
     });
 
     it('Should Display Layer list', () => {
       cy.contains('Layers').should('be.visible');
       cy.get('tamu-gisc-layer-list').should('be.visible');
-      cy.get('tamu-gisc-layer-list-item').should('be.visible');
+      cy.get('tamu-gisc-layer-list-item').should('be.visible').and('not.be.empty');
     });
 
     it('Should Display Legend List', () => {
       cy.contains('Legend').should('be.visible');
       cy.get('tamu-gisc-legend').should('be.visible');
-      cy.get('.legend-info').should('be.visible');
+      cy.get('.legend-info').should('be.visible').and('not.be.empty');
     });
   });
 });
